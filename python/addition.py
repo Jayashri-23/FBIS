ac7a0e6d603c5b02db717c2778fdd12f25880120
Jayashri-23 <jayashrigodge91@gmail.com>
2026-01-21 11:36:34 +0530

    Examples of  operator

---
+++ python/addition.py
@@ -0,0 +1,75 @@
+# ARITHMATI OPERATOR
+# Addition of two number
+a = 2
+b = 20
+
+c= a+b
+print("addition is:",c)
+
+# # subtraction of two number
+c=b-a
+print("substraction is",c)
+
+# # multiplication
+c = a * b
+print("multiplication is:",c)
+
+# # Division 
+c = b / a
+print("Division is:",c)
+
+# # Exponentiation operator
+base=2
+power=3
+print("output is:", base**power)
+
+# Moldulus of two number
+c=a%b   #used to find remainder of the division
+print("modulus is:", c)
+
+# # Take input from user
+name = input("Enter your name:")
+print(name)
+
+# Floor Division of number
+# used to find the Quotient of the division
+c = a // b
+print(c)
+
+# 2. COMPARISON OPERATOR is used to compare two values
+#   i)Equal to operator
+x=20
+y=10
+z = x==y
+print("x is equal to y:" ,z)      # 1st method
+print("x is equal to y :",x==y)  # 2nd method to print
+   
+#  ii)  Less than operator
+print("x is Less than to y :",x<y)
+
+#   iii) Greaterthan operator
+print("x is greater than y :",x>y)
+
+# iv) Lessthan og equal to operator
+print("x is less than or equal to y:" , x<=y)
+
+# v) Greater than or equal to operator
+print("x is greater than or equal to y:", x <= y)
+
+# vi) Not equal to operator
+print("x is not equal to y:", x!=y)
+
+# 3.LOGICAL OPERATOR
+#  i)AND operator
+result = x>50 and y>50
+print("output of AND operator is :" ,result)
+
+# ii) OR operator
+print("The output of OR operator is:" , x<30 or y<5)
+
+# iii) NOT operator
+print("The output of NOT operator is:", not x<2 )
+ 
+result = not(x<2)
+print("resut of NOT operator is:", result)
+
