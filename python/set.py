f3f91641b0bac609614b0f0866b63e8b4765549d
Jayashri-23 <jayashrigodge91@gmail.com>
2026-01-21 11:40:58 +0530

    set methods example

---
+++ python/set.py
@@ -0,0 +1,11 @@
+name1 = {"jayashri" , "khusbu" , "dipti"}
+print(name1)
+name2 = {"1" , "2" , "3"}
+         
+ #Update method
+name1.update(name2)      
+print(name1)
+
+# Union method
+name3 = name1.union(name2)
+print(name3)
