6eca9ea57a9c50dca6d90e94bde6dbcee66cec5e
Jayashri-23 <jayashrigodge91@gmail.com>
2026-01-23 10:46:49 +0530

    simple example solved

--- python/hello.py
+++ python/hello.py
@@ -1,3 +1,14 @@
 print("hello")
 print("world")
 print("Welcome to FutureBright IT Solutions")
+
+# For Loop Example 
+sum = 0
+for i in range(1,10):
+    if i % 2 == 0:
+        print(i)
+        sum += i
+print("Sum is : ",sum)
+
+# Another print method
+print(input("Enter message: "))
